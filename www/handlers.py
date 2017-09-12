7a5a4fc853c87d3da18514e424745c3b4eaa6213
Peijing <chenpeijing666@gmail.com>
2017-09-12 19:12:13 +0800

    web test

--- www/handlers.py
+++ www/handlers.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'Michael Liao'
-
-' url handlers '
-
 import re, time, json, logging, hashlib, base64, asyncio
 
 from coroweb import get, post
